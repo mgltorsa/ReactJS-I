97d9939c65a7d0725114f22f3f2bcddec779fb02
Miguel T <mgltorsa@gmail.com>
2024-04-23 20:50:50 -0400

    agregado los estados locales y globales

--- curso-react/src/App.jsx
+++ curso-react/src/App.jsx
@@ -1,14 +1,77 @@
-import { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./App.css";
 
+//Contador.jsx
+const Contador = () => {
+  const [contador, setContador] = useState(0);
+
+  return (
+    <div>
+      <h1>Cantidad de clicks:</h1>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        {contador}
+      </button>
+    </div>
+  );
+};
+
+const UserContext = React.createContext();
+
+//Encabezado.jsx
+const Encabezado = () => {
+  const user = useContext(UserContext);
+
+  // useEffect(() => {
+  //   console.log("contexto");
+  //   console.log(user);
+  // }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>bienvenido</h1>
+      <button
+        onClick={() => {
+          console.log("USUARIO");
+        }}
+        style={{ borderRadius: 40 }}
+      >
+        <img width={40} height={40} src={user.imagen} />
+      </button>
+    </div>
+  );
+};
+
 function App() {
-  const [mostrar, setMostrar] = useState(false)
+  const user = {
+    nombre: "Miguel",
+    apellido: "Torres",
+    imagen:
+      "https://www.svgrepo.com/show/382095/female-avatar-girl-face-woman-user-4.svg",
+    edad: 23,
+  };
 
   return (
     <>
-      <h1>Test mostrar</h1>
-      <button onClick={() => setMostrar(!mostrar)}>Click para mostrar</button>
-      {mostrar && <p>Existo</p>}
+      {/* <Contador /> */}
+      <UserContext.Provider value={user}>
+        <Encabezado />
+        <div>
+          <h1>Perfil</h1>
+          <p>Nombre: {user.nombre}</p>
+          <p>Apellido: {user.apellido}</p>
+        </div>
+      </UserContext.Provider>
     </>
   );
 }
